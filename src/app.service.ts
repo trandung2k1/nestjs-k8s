5b6a5246d750b94449eed885d072a3b5afb73b6c
trandung2k1 <trandungitksnb2001@gmail.com>
2024-06-29 10:07:23 +0700

    update code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import * as os from 'os';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<h1>I'm ${os.hostname()}</h1>`;
   }
 }
